4f6c2c2205fae7aa346ffd71b43af3ee9e300506
steve590 <stevenmatete4@gmail.com>
2025-03-21 16:56:02 +0300

    Create setting.py
    postgreSQL

---
+++ setting.py
@@ -0,0 +1,10 @@
+DATABASES = {
+	'default': {
+    	'ENGINE': 'django.db.backends.postgresql',
+    	'NAME': 'your_db_name',
+    	'USER': 'your_db_user',
+    	'PASSWORD': 'your_db_password',
+    	'HOST': 'your_db_host',
+    	'PORT': '5432',
+	}
+}
